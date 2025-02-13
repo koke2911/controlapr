468194c9c7f928de48b10e53659f45f06e00a89d
victor <victor.martinez@saludohiggins.cl>
2025-02-13 09:55:28 -0300

    REgulariza regiones

--- modulos/ssr/ssr.php
+++ modulos/ssr/ssr.php
@@ -69,22 +69,22 @@
                                             <label for="cmb_region" class="form-label">Region</label>
                                             <select class="form-control" id="cmb_region" name="cmb_region" disabled required>
                                                 <option value="">Seleccione una Region</option>
-                                                <option value="I">I - Tarapacá</option>
-                                                <option value="II">II - Antofagasta</option>
-                                                <option value="III">III - Atacama</option>
-                                                <option value="IV">IV - Coquimbo</option>
-                                                <option value="V">V - Valparaíso</option>
-                                                <option value="VI">VI - O'Higgins</option>
-                                                <option value="VII">VII - Maule</option>
-                                                <option value="VIII">VIII - Biobío</option>
-                                                <option value="IX">IX - Araucanía</option>
-                                                <option value="X">X - Los Lagos</option>
-                                                <option value="XI">XI - Aysén</option>
-                                                <option value="XII">XII - Magallanes</option>
-                                                <option value="XIII">XIII - Metropolitana</option>
-                                                <option value="XIV">XIV - Los Ríos</option>
-                                                <option value="XV">XV - Arica y Parinacota</option>
-                                                <option value="XVI">XVI - Ñuble</option>
+                                                <option value="I">I</option>
+                                                <option value="II">II</option>
+                                                <option value="III">III</option>
+                                                <option value="IV">IV</option>
+                                                <option value="V">V</option>
+                                                <option value="VI">VI</option>
+                                                <option value="VII">VII</option>
+                                                <option value="VIII">VIII</option>
+                                                <option value="IX">IX</option>
+                                                <option value="X">X</option>
+                                                <option value="XI">XI</option>
+                                                <option value="XII">XII</option>
+                                                <option value="RM">RM</option>
+                                                <option value="XIV">XIV</option>
+                                                <option value="XV">XV</option>
+                                                <option value="XVI">XVI</option>
                                             </select>
                                         </div>
                                     </div>

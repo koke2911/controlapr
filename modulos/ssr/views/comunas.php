b07111f7fb5fc398a1e70e0ab2ca18ecf6fa29db
victor <victor.martinez@saludohiggins.cl>
2025-02-24 08:02:09 -0300

    errores

--- modulos/ssr/views/comunas.php
+++ modulos/ssr/views/comunas.php
@@ -3,6 +3,8 @@ session_start();
 
 $conn = new mysqli($_SESSION['servername'], $_SESSION['username'], $_SESSION['password'], $_SESSION['dbname']);
 
+mysqli_set_charset($conn, 'utf8mb4');
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -14,7 +16,11 @@ $data = array();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $row["comuna"] = preg_replace('/[^A-Za-z0-9\-]/', '', $row["comuna"]);
+        $row["comuna"] = preg_replace(
+            '/[^A-Za-z0-9\-\'\xE1\xE9\xED\xF1\xF3\xFA]/u',
+            '',
+            $row["comuna"]
+        );
         array_push($data, array('id' => $row["id"], 'comuna' => $row["comuna"]));
     }
 }
